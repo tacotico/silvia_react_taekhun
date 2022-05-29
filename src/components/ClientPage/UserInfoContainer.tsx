0b27a6a00e9b034eecf0cc4982982fd6f1ba2715
Taekhun <nth9708@naver.com>
2022-05-29 23:57:34 +0900

    refactor: UserInfo Component eject

---
+++ src/components/ClientPage/UserInfoContainer.tsx
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+import Icon from '@components/commons/Icon';
+import { UserInfoKeys, UserInfoType } from '@customTypes/user';
+import { userInfoKor } from '@lib/config/constants';
+
+interface Props {
+  userInfo: UserInfoType | null;
+}
+
+const UserInfoContainer = ({ userInfo }: Props) => {
+  const userInfoList =
+    userInfo && Object.entries(userInfo).filter(([key]) => key !== 'nickname' && key !== 'profileImg');
+
+  return (
+    userInfo && (
+      <Wrapper>
+        <ProfileImageWrapper>
+          <Icon src={userInfo.profileImg} alt='profile-image' width='10rem' style={{ borderRadius: '50%' }} />
+        </ProfileImageWrapper>
+
+        <UserInfoBox>
+          <InfoBoxRow1>
+            <Nickname>{userInfo.nickname}</Nickname>
+            <NicknameEditButton>button</NicknameEditButton>
+          </InfoBoxRow1>
+          <InfoBoxRow2>
+            {userInfoList?.map(([label, data], key) => (
+              <TextWrapper key={key}>
+                <Label>{userInfoKor[label as UserInfoKeys]}</Label>
+                <Text>{data}</Text>
+              </TextWrapper>
+            ))}
+          </InfoBoxRow2>
+        </UserInfoBox>
+      </Wrapper>
+    )
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 4.4rem;
+  width: 100%;
+  position: relative;
+  margin-bottom: 4rem;
+  padding: 0 ${({ theme }) => theme.padding.contentBox};
+  flex-wrap: nowrap;
+  overflow-x: auto;
+`;
+
+const ProfileImageWrapper = styled.div`
+  border-radius: 50%;
+`;
+
+const UserInfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1.2rem;
+  flex: 0 0 auto;
+`;
+
+const InfoBoxRow1 = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2.4rem;
+`;
+
+const InfoBoxRow2 = styled.div`
+  display: flex;
+  font-size: 1.6rem;
+  gap: 5.6rem;
+`;
+
+const Nickname = styled.h2`
+  font-size: 3rem;
+  font-weight: 500;
+`;
+const NicknameEditButton = styled.button``;
+
+const TextWrapper = styled.div`
+  display: flex;
+  gap: 1.2rem;
+  align-items: center;
+`;
+const Label = styled.span`
+  color: rgba(0, 0, 0, 0.45);
+`;
+const Text = styled.span``;
+
+const BgPattern = styled(Icon)`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+export default UserInfoContainer;
--- src/pages/ClientPage.tsx
+++ src/pages/ClientPage.tsx
@@ -1,121 +1,68 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { PageHeader } from 'antd';
 
-import Icon from '@components/commons/Icon';
 import bgPatternSrc from '@assets/icons/bgPattern.svg';
-import { ROUTES, userInfoKor } from '@lib/config/constants';
-import { UserInfoKeys } from '@customTypes/user';
+import { ROUTES } from '@lib/config/constants';
+import { UserInfoType } from '@customTypes/user';
 import { useNavigate } from 'react-router';
+import UserInfoContainer from '@components/ClientPage/UserInfoContainer';
+import SessionListContainer from '@components/ClientPage/SessionListContainer';
+import { getUserInfo } from '@lib/api/user';
+import { getSessionList } from '@lib/api/session';
+import { SessionType } from '@customTypes/session';
+import StatisticsListContainer from '@components/ClientPage/StatisticsListContainer';
+import ContentUsageGraphContainer from '@components/ClientPage/GraphContainer';
 
 interface Props {}
 
 const ClientPage = (props: Props) => {
+  const [userId, setUserId] = useState<string>('user1');
+  const [userInfo, setUserInfo] = useState<UserInfoType | null>(null);
+  const [sessionList, setSessionList] = useState<SessionType[] | []>([]);
+
   const navigate = useNavigate();
 
   const handleBackClick = useCallback(() => {
     navigate(ROUTES.home);
   }, [navigate]);
 
-  const userInfo = {
-    nickname: '남택훈',
-    age: '12',
-    tel: '010-0000-0000',
-    sex: '남성',
-    birth: '1997.08.13',
-    profileImg: 'https://avatars.githubusercontent.com/u/19265753?v=4',
-  };
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const res = await getUserInfo(userId);
+      res && setUserInfo(res);
+    };
 
-  const userInfoList = Object.entries(userInfo).filter(([key]) => key !== 'nickname' && key !== 'profileImg');
+    fetchUserInfo();
+  }, [userId]);
+
+  useEffect(() => {
+    const fetchSessionList = async () => {
+      const res = await getSessionList(userId);
+      res && setSessionList(res);
+    };
+
+    fetchSessionList();
+  }, [userId]);
 
   return (
     <Wrapper>
       <Header title='내담자 대시보드' onBack={handleBackClick} />
-      <UserInfoContainer>
-        <ProfileImageWrapper>
-          <Icon src={userInfo.profileImg} alt='profile-image' width='10rem' style={{ borderRadius: '50%' }} />
-        </ProfileImageWrapper>
-
-        <UserInfoBox>
-          <InfoBoxRow1>
-            <Nickname>{userInfo.nickname}</Nickname>
-            <NicknameEditButton>button</NicknameEditButton>
-          </InfoBoxRow1>
-          <InfoBoxRow2>
-            {userInfoList.map(([label, data], key) => (
-              <TextWrapper key={key}>
-                <Label>{userInfoKor[label as UserInfoKeys]}</Label>
-                <Text>{data}</Text>
-              </TextWrapper>
-            ))}
-          </InfoBoxRow2>
-        </UserInfoBox>
-      </UserInfoContainer>
-      {/* <BgPattern width='33rem' src={bgPatternSrc} alt='background_pattern' /> */}
+      <UserInfoContainer userInfo={userInfo} />
+      <SessionListContainer sessionList={sessionList} />
+      <StatisticsListContainer />
+      <ContentUsageGraphContainer />
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  padding: ${({ theme }) => theme.padding.contentBox};
   background: none;
 `;
 
 const Header = styled(PageHeader)`
-  padding: 0;
   margin-bottom: 2.4rem;
-`;
-
-const UserInfoContainer = styled.div`
-  display: flex;
-  gap: 4.4rem;
-  width: 100%;
-  position: relative;
-`;
-
-const ProfileImageWrapper = styled.div`
-  border-radius: 50%;
-`;
-
-const UserInfoBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 1.2rem;
-`;
-
-const InfoBoxRow1 = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 2.4rem;
-`;
-
-const InfoBoxRow2 = styled.div`
-  display: flex;
-  font-size: 1.6rem;
-  gap: 5.6rem;
-`;
-
-const Nickname = styled.h2`
-  font-size: 3rem;
-  font-weight: 500;
-`;
-const NicknameEditButton = styled.button``;
-
-const TextWrapper = styled.div`
-  display: flex;
-  gap: 1.2rem;
-  align-items: center;
-`;
-const Label = styled.span`
-  color: rgba(0, 0, 0, 0.45);
-`;
-const Text = styled.span``;
-
-const BgPattern = styled(Icon)`
-  position: absolute;
-  top: 0;
-  right: 0;
+  padding: ${({ theme }) => theme.padding.contentBox};
 `;
 
 export default ClientPage;
